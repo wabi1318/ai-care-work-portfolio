186a4e0e0b942aa1484c5cbc672aa72e8b275334
wabi <wabi1318.wabi@gmail.com>
2025-05-18 18:11:15 +0900

    修正

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,4 +1,9 @@
 import path from "path"; // path モジュールをインポート
+import { fileURLToPath } from "url";
+
+// ES Modulesでは __filename や __dirname は直接利用できないため、import.meta.urlから取得する
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
